48dca07e39ea763a563dff4cd882a036422e3cb5
taesunmi <taesunmi@192.168.42.48>
2025-01-15 20:07:57 +0900

    25.1.15 마지막 커밋

---
+++ CafeProject/src/main/java/Service/FreeBoard/IFreeBoardService.java
@@ -0,0 +1,31 @@
+package Service.FreeBoard;
+import java.util.List;
+
+import VO.FreeBoardVO;
+
+/**
+ * 파라미터
+ * 1. 단순하게 사용자 ID 하나를 가지고 사용자의 정보 조회를 하는 경우 : userID만 받음
+ * 2. 여러 정보를 가지고 사용자의 정보를 업데이트 해야하는 경우 : VO로 받음
+ * 3. 전체 데이터를 조회하거나 특정 조건에 따른 필터링 없이 모든 데이터를 반환해야하는 경우 : List(파라미터없음) 사용
+ * 4. 전체 데이터를 조회하는데 특정 조건에 따른 필터링이 필요한 경우 : List(조건파라미터) 사용
+ */
+
+public interface IFreeBoardService {
+	// 게시글 작성 (insert)
+	public int insertFreeBoard(FreeBoardVO FreeBoardVO);
+	
+	// 게시글 수정 (update)
+	public int updateFreeBoard(FreeBoardVO FreeBoardVO);
+	
+	// 게시글 삭제 (delete)
+	public int deleteFreeBoard(int boardNo);
+	
+	// 게시글 조회 (select - 사용자가 특정 게시글 검색)
+	// 글번호, 제목, 내용, 작성일, 조회수로 게시글 조회 가능
+	public List<FreeBoardVO> selectFreeBoard(FreeBoardVO FreeBoardVO);
+	
+	// 게시글 전체조회 (select - 게시판 들어갔을 때 전체게시글 목록)
+	public List<FreeBoardVO> selectAllFreeBoard();
+	
+}
--- CafeProject/src/main/java/VO/FreeBoardVO.java
+++ CafeProject/src/main/java/VO/FreeBoardVO.java
@@ -3,41 +3,59 @@ package VO;
 import java.util.Objects;
 
 // 1. 값을 저장할 수 있는 프로퍼티 정의 + 캡슐화(private)
-public class freeBaordVO {
-	 private int boardNo;
-	 private String boardTitle;
-	 private String boardContent;
-	 private int boardHits;
-	 private String memId;
+public class FreeBoardVO {
+	 private int boardNo; // 글번호
+	 private String boardTitle; // 제목
+	 private String boardContent; // 게시글 내용
+	 private String boardDate; // 작성일
+	 private int boardHits; // 조회수
+	 private String memId; // 회원ID
 
 // 2. 캡슐화된 프로퍼티에 접근 가능한 인터페이스 메소드 정의
 	public int getBoardNo() {
 		return boardNo;
 	}
+
 	public void setBoardNo(int boardNo) {
 		this.boardNo = boardNo;
 	}
+
 	public String getBoardTitle() {
 		return boardTitle;
 	}
+
 	public void setBoardTitle(String boardTitle) {
 		this.boardTitle = boardTitle;
 	}
+
 	public String getBoardContent() {
 		return boardContent;
 	}
+
 	public void setBoardContent(String boardContent) {
 		this.boardContent = boardContent;
 	}
+
+	public String getBoardDate() {
+		return boardDate;
+	}
+
+	public void setBoardDate(String boardDate) {
+		this.boardDate = boardDate;
+	}
+
 	public int getBoardHits() {
 		return boardHits;
 	}
+
 	public void setBoardHits(int boardHits) {
 		this.boardHits = boardHits;
 	}
+
 	public String getMemId() {
 		return memId;
 	}
+
 	public void setMemId(String memId) {
 		this.memId = memId;
 	}
@@ -47,7 +65,6 @@ public class freeBaordVO {
 	public int hashCode() {
 		return Objects.hash(boardNo);
 	}
-	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -56,19 +73,14 @@ public class freeBaordVO {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		freeBaordVO other = (freeBaordVO) obj;
+		FreeBoardVO other = (FreeBoardVO) obj;
 		return boardNo == other.boardNo;
 	}
 
 // 4. 상태를 확인할 수 있는 메소드 정의
 	@Override
 	public String toString() {
-		return "freeBaordVO [boardNo=" + boardNo + ", boardTitle=" + boardTitle + ", boardContent=" + boardContent
-				+ ", boardHits=" + boardHits + ", memId=" + memId + "]";
+		return "FreeBoardVO [boardNo=" + boardNo + ", boardTitle=" + boardTitle + ", boardContent=" + boardContent
+				+ ", boardDate=" + boardDate + ", boardHits=" + boardHits + ", memId=" + memId + "]";
 	}
-	
-	
-	
-	 
-	 
 }
